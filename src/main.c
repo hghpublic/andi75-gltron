5bfa85482162f0662c5b7f0b1b7e16457f9c750e
hghdev <hgh.dev@t-online.de>
2024-10-28 19:36:16 +0100

    Added files.

---
+++ src/main.c
@@ -0,0 +1,17 @@
+// main.c
+
+#include "libmodplug/modplug.h"
+#include "lua.h"
+#include "mikmod.h"
+#include "ogg/ogg.h"
+#include "png.h"
+#include "SDL2/SDL.h"
+#include "vorbis/vorbisfile.h"
+#include "zlib.h"
+#include "SDL_sound.h"
+#include "dirent.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
