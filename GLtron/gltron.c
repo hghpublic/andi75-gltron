6ab6841f8ae2f0ac5b43ddbb5c4e37aa3d80d37e
hghdev <hgh.dev@t-online.de>
2024-10-28 15:07:22 +0100

    Added changing to exe directory at start of program.
    This allows to start and debug gtron from within the Visual Studio IDE.

--- GLtron/gltron.c
+++ GLtron/gltron.c
@@ -12,7 +12,34 @@
 #include "filesystem/path.h"
 #include "base/util.h"
 
+#if defined(WIN32)
+#include <direct.h>
+#include <errno.h>
+#include <SDL.h>
+void chdirToBasePath()
+{
+    const char* basePath = SDL_GetBasePath();
+    if (_chdir(basePath))
+    {
+        switch (errno)
+        {
+        case ENOENT:
+            printf("Unable to locate the directory: %s\n", basePath);
+            break;
+        case EINVAL:
+            printf("Invalid buffer.\n");
+            break;
+        default:
+            printf("Unknown error.\n");
+        }
+    }
+}
+#endif
+
 int main(int argc, char *argv[] ) {
+#if defined(WIN32)
+    chdirToBasePath();
+#endif
 	// nebu_debug_memory_CheckLeaksOnExit();
 	// nebu_assert_config(NEBU_ASSERT_PRINT_STDERR);
 	initSubsystems(argc, (const char**) argv);
