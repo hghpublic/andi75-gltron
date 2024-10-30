fe286e4d9614aee9747250822f450e1cbcee1a19
hghdev <hgh.dev@t-online.de>
2024-10-30 11:55:47 +0000

    Fixed typo.

--- GLtron/nebu/include/video/nebu_geometry.h
+++ GLtron/nebu/include/video/nebu_geometry.h
@@ -1,7 +1,7 @@
 #ifndef _NEBU_GEOMETRY_H
 #define _NEBU_GEOMETRY_H
 
-#include "video/nebu_Mesh.h"
+#include "video/nebu_mesh.h"
 
 nebu_Mesh* nebu_geom_CreateCylinder(float radius, float radius2, float height, int slices, int stacks);
 
